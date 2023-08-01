de6be40f2b4577b34aaa0d8710ff98f4102b7c4e
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-01 13:44:24 -0400

    global context set-up

--- src/componenets/context.jsx
+++ src/componenets/context.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import sublinks from "../data";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsideBarOpen] = useState(true);
+  const [isModalOpen, setModalOpen] = useState(true);
+
+  const openSideBar=()=>{
+    setIsSideBarOpen(true)
+  }
+  const closeSideBar=()=>{
+setIsSideBarOpen(false)
+  }
+  const openModal=()=>{
+    setiSModalOpen(true)
+  }
+  const closeModal=()=>{
+setIsModalOpen(false)
+
   return <AppContext.Provider>{children}</AppContext.Provider>;
 };
