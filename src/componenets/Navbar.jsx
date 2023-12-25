aa814beff6dc7b14839ec7cdfb29030d5861c253
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-12-24 23:28:23 -0500

    Navbar comp set up

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,5 +1,35 @@
-function Navbar() {
-  return <></>;
-}
+import React from "react";
+import { useGlobalContext } from "./context";
+import { FaBars } from "react-icons/fa";
+import logo from "./images/logo.svg";
+
+const Navbar = () => {
+  const { openSideBar, openSubMenu, closeSubMenu } = useGlobalContext();
+
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <img src={logo} className="nav-logo" alt="stripe" />
+        <button className="btn toggle-btn" onClick={openSideBar}>
+          <FaBars />
+        </button>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <button className="link-btn">products</button>
+        </li>
+        <li>
+          <button className="link-btn">developers</button>
+        </li>
+        <li>
+          <button className="link-btn">company</button>
+        </li>
+        <li>
+          <button className="link-btn">Sign in</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
