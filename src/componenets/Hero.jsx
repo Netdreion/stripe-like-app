e5826d4488d4089c2e53fc38f48e3277f49397a2
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-12-26 21:51:02 -0500

    sidebar comp added

--- src/componenets/Hero.jsx
+++ src/componenets/Hero.jsx
@@ -9,7 +9,14 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-center">
-        <article></article>
+        <article className="hero-info">
+          <h1>payments insfructure for internet </h1>
+          <p>
+            millions of companies of all sizes-from startups to fortunene
+            500s-use Stripe software and APIs to accept payments, send payouts,
+            and manage their business online
+          </p>
+        </article>
         <article className="hero-images">
           <img src={phoneImg} className="phone-img" alt="phone" />
         </article>
